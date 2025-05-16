e391beada705af5aec7743379a97cc9f887138fd
Jonathan Mauricenz <jonathan.mauricenz@gmail.com>
2025-05-15 22:51:53 -0300

    feat: adds json-server and configuration

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,3 @@
-// Constantes para APIs REST
+// Constantes para APIs REST e GraphQL
+export const REST_API_URL = 'http://localhost:3001/posts'
 export const GRAPHQL_API_URL = 'http://localhost:3002/graphql'
